6bc0dd5fa765aa67ef48dda6cad1aeac9b22613e
ThePetrovich <petroff.ap@gmail.com>
2020-12-19 02:36:10 +0900

    Fixed assembly routines im so stupid lol

--- kernel/arch/mega128/interrupts.c
+++ kernel/arch/mega128/interrupts.c
@@ -11,31 +11,7 @@
 #include <arch/mega128/stack.h>
 #include <arch/mega128/mega128.h>
 
-void tasks_tick();
-void tasks_switchTask();
-
-void __attribute__ (( naked )) _arch_save_context();
-void __attribute__ (( naked )) _arch_restore_context();
-
-void __attribute__ (( naked, noinline )) arch_yield(void)
-{
-	_arch_save_context();
-
-	tasks_switchTask();
-
-	_arch_restore_context();
-	arch_RET();
-}
-
-void __attribute__ (( naked, noinline )) arch_tick()
-{
-	_arch_save_context();
-
-	tasks_tick();
-
-	_arch_restore_context();
-	arch_RET();
-}
+void __attribute__ (( naked, noinline )) arch_tick();
 
 ISR(TIMER0_COMP_vect, ISR_NAKED)
 {
