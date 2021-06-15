9b686fee13c7b2a96e67e8e6d2ddaa81498441a3
ThePetrovich <petroff.ap@gmail.com>
2021-06-15 20:38:14 +0900

    Arch info on init

--- include/kernel/arch/arch.h
+++ include/kernel/arch/arch.h
@@ -10,11 +10,14 @@
 #define KERNEL_MODULE_ARCH_H_
 
 #ifdef KERNEL_ARCH_avr
+	#define KERNEL_ARCH "avr"
 	#if defined KERNEL_MCU_atmega128
+		#define KERNEL_MCU "atmega128"
 		#include "./mega128/arch.h"
 		#include "./mega128/uart.h"
 		#include "./mega128/stack.h"
 	#elif defined KERNEL_MCU_atmega2560
+		#define KERNEL_MCU "atmega2560"
 		#include "./mega2560/arch.h"
 		#include "./mega2560/uart.h"
 		#include "./mega2560/stack.h"
@@ -23,6 +26,14 @@
 	#endif
 #else
 	#error "Unsupported/unknown architecture"
+#endif
+
+#ifndef KERNEL_ARCH
+#define KERNEL_ARCH "unkn"
+#endif
+
+#ifndef KERNEL_MCU
+#define KERNEL_MCU "unkn"
 #endif
 
 #endif
