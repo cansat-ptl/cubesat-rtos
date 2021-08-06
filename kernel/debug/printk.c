9f01f7fc3c0e518fc41aeb403a9c789a9f70c13c
Anatoly Antonov <awendo@mail.ru>
2021-08-06 17:59:41 +0900

    POSIX codestyle > kernel codestyle

--- kernel/debug/printk.c
+++ kernel/debug/printk.c
@@ -5,7 +5,6 @@
  * Author: awend0
  */
 
-
 #include <stdarg.h>
 #include <kernel/types.h>
 #include <kernel/debug/printk.h>
@@ -18,11 +17,11 @@ static int debug_spacing(int count, char c)
 {
 	int ret = 0;
 
-	while (ret < count) {
+	while (ret < count)
+	{
 		uart_putc(c);
 		ret++;
 	}
-
 	return (ret);
 }
 
@@ -33,18 +32,17 @@ static int debug_putnbr(long number, int base, int len, char *alph)
 
 	sign = (number < 0) ? -1 : 1;
 	number *= sign;
-
 	len--;
-	while (len >= 0) {
+	while (len >= 0)
+	{
 		str[len] = alph[number % base];
 		number /= base;
 		len--;
 	}
-
-	if (sign == -1) {
+	if (sign == -1)
+	{
 		str[0] = '-';
 	}
-
 	uart_puts(str);
 	return (common_strlen(str));
 }
@@ -53,19 +51,19 @@ static int debug_nbrlen(long nbr, int base)
 {
 	int	len = 0;
 
-	if (nbr == 0) {
+	if (nbr == 0)
+	{
 		return (1);
 	}
-
-	if (nbr < 0 && base == 10) {
+	if (nbr < 0 && base == 10)
+	{
 		len += 1;
 	}
-
-	while (nbr) {
+	while (nbr)
+	{
 		nbr /= base;
 		len += 1;
 	}
-
 	return (len);
 }
 
@@ -76,16 +74,15 @@ static int debug_print_d(char **format, int width, int zero_flag, va_list args)
 	int ret = 0;
 
 	(*format)++;
-	if (width == -1) {
+	if (width == -1)
+	{
 		width = len;
 	}
-
-	if (width > len) {
+	if (width > len)
+	{
 		ret += debug_spacing(width - len, (zero_flag) ? '0' : ' ');
 	}
-
 	ret += debug_putnbr(value, 10, len, "0123456789");
-
 	return (ret);
 }
 
@@ -96,16 +93,15 @@ static int debug_print_x(char **format, int width, int zero_flag, int reg, va_li
 	int ret = 0;
 
 	(*format)++;
-	if (width == -1) {
+	if (width == -1)
+	{
 		width = len;
 	}
-
-	if (width > len) {
+	if (width > len)
+	{
 		ret += debug_spacing(width - len, (zero_flag) ? '0' : ' ');
 	}
-
 	ret += debug_putnbr(value, 16, len, (reg == 0) ? "0123456789ABCDEF" : "0123456789abcdef");
-
 	return (ret);
 }
 
@@ -116,21 +112,20 @@ static int debug_print_s(char **format, int width, int zero_flag, va_list args)
 	int ret = 0;
 
 	(*format)++;
-	if (!str) {
+	if (!str)
+	{
 		str = "(null)";
 	}
-
 	len = common_strlen(str);
-	if (width == -1) {
+	if (width == -1)
+	{
 		width = len;
 	}
-
-	if (width > len) {
+	if (width > len)
+	{
 		ret += debug_spacing(width - len, (zero_flag) ? '0' : ' ');
 	}
-
 	uart_puts(str);
-
 	return (ret + len);
 }
 
@@ -138,11 +133,11 @@ static int debug_parse_flag(char **format)
 {
 	int ret = 0;
 
-	if (**format == '0') {
+	if (**format == '0')
+	{
 		ret = 1;
 		(*format)++;
 	}
-
 	return (ret);
 }
 
@@ -150,15 +145,16 @@ static int debug_parse_width(char **format)
 {
 	int ret = -1;
 
-	if (common_isdigit(**format)) {
+	if (common_isdigit(**format))
+	{
 		ret = 0;
-		while (common_isdigit(**format)) {
+		while (common_isdigit(**format))
+		{
 			ret *= 10;
 			ret += **format - '0';
 			(*format)++;
 		}
 	}
-
 	return (ret);
 }
 
@@ -171,8 +167,8 @@ static int debug_printk_processor(char **format, va_list args)
 	(*format)++;
 	zero_flag = debug_parse_flag(format);
 	width = debug_parse_width(format);
-
-	switch(**format) {
+	switch(**format)
+	{
 	case 'd':
 		ret = debug_print_d(format, width, zero_flag, args);
 		break;
@@ -193,7 +189,6 @@ static int debug_printk_processor(char **format, va_list args)
 		ret = 0;
 		break;
 	}
-
 	return (ret);
 }
 
@@ -201,12 +196,12 @@ static int debug_write_till_percent(char **format)
 {
 	int ret = 0;
 
-	while (**format && **format != '%') {
+	while (**format && **format != '%')
+	{
 		uart_putc(**format);
 		ret++;
 		(*format)++;
 	}
-
 	return (ret);
 }
 
@@ -216,9 +211,9 @@ int debug_printk(char *format, ...)
 
 	int ret = 0;
 	kStatusRegister_t sreg = arch_enterCriticalSectionSafe();
-
 	va_start(args, format);
-	while (*format) {
+	while (*format)
+	{
 		ret += debug_write_till_percent(&format);
 
 		if (*format == '%') {
@@ -226,9 +221,6 @@ int debug_printk(char *format, ...)
 		}
 	}
 	va_end(args);
-	
 	arch_exitCriticalSectionSafe(sreg);
-
-
 	return (ret);
 }
